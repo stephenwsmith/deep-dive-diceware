ca62ef1beb5452302f8df09c3b0c37b1edc1fa40
Stephen Smith <steve@newmexico.com>
2018-06-01 12:50:37 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/diceware/Generator.java
@@ -0,0 +1,94 @@
+package edu.cnm.deepdive.diceware;
+
+/**
+ * to generate random array of words.
+ *
+ * takes input and produces a randow list of words based on list as output
+ *
+ * Import packages
+ */
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+/**
+ *  generate error messages
+ */
+
+public class Generator {
+
+  private static final String NULL_RNG_MESSAGE = "Random number gen must not be null.";
+  private static final String NEG_NUM_MESSAGE = "Number of words cannot be negative.";
+  private static final String INSUFFICENT_WORDS = "Insufficent words";
+  private static final String CANNOT_BE_NULL = "WORDS_CANNOT_BE_NULL.";
+  private static final String NO_DUPLICATESS_ALLOWED = "No Duplicatess Allowed!";
+
+  public String[] words;
+  //public Random rng = word;  TODO fix this?
+
+  /**
+   * begin
+   *
+   */
+
+  public Generator(String[] words, Random rng) {
+
+// exception if rng is invalid
+    if (rng == null) {
+      throw new NullPointerException(NULL_RNG_MESSAGE);
+    }
+// exception if words entered are empty
+    if (words == null) {
+      throw new NullPointerException(CANNOT_BE_NULL);
+    }
+    //exception if array given is negative
+    if (words.length == 0) {
+      throw new NegativeArraySizeException(NEG_NUM_MESSAGE);
+    }
+    //exception if not enough words given
+    if (numWords == 0) {
+      throw new IllegalArgumentException(INSUFFICENT_WORDS);
+    }
+    //exception if no dups are allowed
+    if (!DupicatesAllowed) && numWords > 0;{
+      throw new IllegalArgumentException(NO_DUPLICATESS_ALLOWED);
+    }
+    //convert  quanitiy of words to lower case, make an array
+    Set<String> pool = new HashSet<>();
+    {
+      for (String word : words) {
+        word = word.toLowerCase();
+        if (!pool.contains(word)) {
+          pool.add(word);
+        }
+      }
+      this.words = pool.toArray(new String[pool.size()]);
+      this.rng = rng;
+    }//TODO finish this
+
+    }
+//test for dups allowed, and output if no dupsallowed but dups exist
+    public String[] next ( int numWords, boolean duplicatesAllowed){
+      List<String> selection = new LinkedList<>();
+      while (selection.size() < numWords) {
+        String pick = next();
+        if (duplicatesAllowed || !selection.contains(pick)) {
+          selection.add(pick);
+        }
+      }
+      return selection.toArray(new String[selection.size()]);
+    }
+//return words if dups are allowed
+    public String[] next ( int numWords){
+
+      return next(numWords, true);
+    }
+  }
+
+
+
+
